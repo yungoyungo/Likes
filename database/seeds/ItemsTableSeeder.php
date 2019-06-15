e34b7cd01696dbfb5abd223732635dfd92be0600
yungo <29724679+yungoyungo@users.noreply.github.com>
2019-06-15 18:31:47 +0700

    item.seeder変更

--- database/seeds/ItemsTableSeeder.php
+++ database/seeds/ItemsTableSeeder.php
@@ -28,5 +28,45 @@ class ItemsTableSeeder extends Seeder
             'image_path' => 'sample/リライト.jpg',
             'user_id' => '2',
         ]);
+        App\Item::create([
+            'title' => 'フィードバックファイル2',
+            'image_path' => 'sample/フィードバックファイル2.jpg',
+            'user_id' => '2',
+        ]);
+        App\Item::create([
+            'title' => '藤沢ルーザー',
+            'image_path' => 'sample/藤沢ルーザー.jpg',
+            'user_id' => '2',
+        ]);
+        App\Item::create([
+            'title' => 'ホームタウン',
+            'image_path' => 'sample/ホームタウン.jpg',
+            'user_id' => '2',
+        ]);
+        App\Item::create([
+            'title' => 'ループ&ループ',
+            'image_path' => 'sample/ループ&ループ.jpg',
+            'user_id' => '2',
+        ]);
+        App\Item::create([
+            'title' => '今を生きて',
+            'image_path' => 'sample/今を生きて.jpg',
+            'user_id' => '2',
+        ]);
+        App\Item::create([
+            'title' => '荒野を歩け',
+            'image_path' => 'sample/荒野を歩け.jpg',
+            'user_id' => '2',
+        ]);
+        App\Item::create([
+            'title' => 'dororo/解放区',
+            'image_path' => 'sample/dororo:解放区.jpg',
+            'user_id' => '2',
+        ]);
+        App\Item::create([
+            'title' => 'ブラッドサーキュレーター',
+            'image_path' => 'sample/ブラッドサーキュレーター.jpg',
+            'user_id' => '2',
+        ]);
     }
 }
