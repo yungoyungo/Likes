30089ae546f8b1775a00658a4ccc85c199443d8f
yungo <29724679+yungoyungo@users.noreply.github.com>
2019-06-21 16:54:22 +0700

    ブランドロゴを画像に変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,18 +21,20 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-light shadow-sm" style="background-color: #ADD">
             <div class="container">
                 @auth
                     <a class="navbar-brand" href="{{ route('user.items.index', ['user' => auth()->user()]) }}">
                         <!-- {{ config('app.name', 'Laravel') }} -->
-                        スコ
+                        <!-- スコ -->
+                        <img src="/storage/suko.png", alt="suko_logo" class="img-fluid" style="height:2em;">
                     </a>
                 @endauth
                 @guest
                     <a class="navbar-brand" href="{{ route('login') }}">
                         <!-- {{ config('app.name', 'Laravel') }} -->
-                        スコ
+                        <!-- スコ -->
+                        <img src="/storage/suko.png", alt="suko_logo" class="img-fluid" style="height:3em;">
                     </a>
                 @endguest
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
