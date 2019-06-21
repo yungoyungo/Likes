906b066f927ddbcc152fc172e9c777ed4da9d5a3
yungo <29724679+yungoyungo@users.noreply.github.com>
2019-06-21 21:23:54 +0700

    ロゴ画像の格納場所変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,14 +27,14 @@
                     <a class="navbar-brand" href="{{ route('user.items.index', ['user' => auth()->user()]) }}">
                         <!-- {{ config('app.name', 'Laravel') }} -->
                         <!-- スコ -->
-                        <img src="/storage/suko.png", alt="suko_logo" class="img-fluid" style="height:2em;">
+                        <img src="{{ asset('images/suko.png') }}", alt="suko_logo" class="img-fluid" style="height:2em;">
                     </a>
                 @endauth
                 @guest
                     <a class="navbar-brand" href="{{ route('login') }}">
                         <!-- {{ config('app.name', 'Laravel') }} -->
                         <!-- スコ -->
-                        <img src="/storage/suko.png", alt="suko_logo" class="img-fluid" style="height:3em;">
+                        <img src="{{ asset('images/suko.png') }}", alt="suko_logo" class="img-fluid" style="height:3em;">
                     </a>
                 @endguest
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
