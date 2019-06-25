a9931840b489501e466028be933be8dc9d99955b
yungo <29724679+yungoyungo@users.noreply.github.com>
2019-06-25 10:11:17 +0700

    createにプレビュー追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,9 +19,13 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/fade.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/fileInput.css') }}" rel="stylesheet">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="{{ asset('js/fade.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('js/fileInput.js') }}"></script>
 </head>
 <body class="fadeout">
     <div id="app">
