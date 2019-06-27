5bb151add28ac7004bed87de368e5d4566c999d5
yungo <29724679+yungoyungo@users.noreply.github.com>
2019-06-27 12:08:05 +0700

    プルダウン動かなかったのを修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,7 @@
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> -->
     <script type="text/javascript" src="{{ asset('js/fade.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/fileInput.js') }}"></script>
 </head>
